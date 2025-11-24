4d236415709359fd9860645661b0de8240c51e94
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2025-11-25 00:51:30 +1300

    Add fixes for profiling
    See https://github.com/IntersectMBO/plutus/issues/7415#issuecomment-3531989244
    See https://gitlab.haskell.org/ghc/ghc/-/merge_requests/15096

--- overlays/bootstrap.nix
+++ overlays/bootstrap.nix
@@ -338,6 +338,12 @@ in {
                 ++ onGhcjs (from        "9.13"         ./patches/ghc/ghc-9.13-ghcjs-rts-types.patch)
 
                 ++ onGhcjs (fromUntil   "9.6.7" "9.7"  ./patches/ghc/ghc-9.6-js-support-this-unit-id-10819.patch)
+
+                # See https://github.com/IntersectMBO/plutus/issues/7415#issuecomment-3531989244
+                ++ fromUntil "9.6" "9.9" ./patches/ghc/ghc-profiling-fix.patch
+
+                # See https://gitlab.haskell.org/ghc/ghc/-/merge_requests/15096
+                ++ fromUntil "9.6" "9.13" ./patches/ghc/ghc-16bit-elf-section-header-overflow.patch
                 ;
         in ({
             ghc8107 = traceWarnOld "8.10" (final.callPackage ../compiler/ghc {
